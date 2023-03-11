c31b01e21264fea846002bd3c00d08a4a7eea3ee
“sendusyourbones” <beckee.roos@gmail.com>
2023-03-10 17:10:55 -0800

    Add function to show current day

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
   // Get HTML elements
   const $saveButton = $('.saveBtn');
   const $hourDivs = $('.time-block');
+  const $currentDay = $('#currentDay');
 
   // When save button is clicked
   $saveButton.on('click', function() {
@@ -34,13 +25,6 @@ $(document).ready(function () {
     localStorage.setItem('tasks', JSON.stringify(tasks));
   });
 
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
   function applyClass() {
     // Get the current hour
     const currentHour = dayjs().hour();
@@ -61,11 +45,6 @@ $(document).ready(function () {
     });
   }
   applyClass();
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
 
   // Get tasks from local storage
   let tasks = JSON.parse(localStorage.getItem('tasks'));
@@ -87,5 +66,13 @@ $(document).ready(function () {
 
   showTasks();
 
-  // TODO: Add code to display the current date in the header of the page.
+  function showCurrentDay() {
+    // Get current day and modify format ex. Friday, March 20, 2023
+    const currentDay = dayjs().format('dddd, MMMM D, YYYY');
+    
+    // Set text of currentDay element to the above
+    $currentDay.text(currentDay);
+  }
+
+  showCurrentDay();
 });
